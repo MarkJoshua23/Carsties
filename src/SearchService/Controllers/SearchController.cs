b9bdca27e792d20be7ea4f3074f9c5109271bbd0
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-02-07 16:57:45 +0800

    filters

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -11,15 +12,61 @@ public class SearchController : ControllerBase
 {
     //get search items
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    //[FromQuery] to make it look in the url
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.Find<Item>();
-        query.Sort(x => x.Ascending(a => a.Make));
-        if (!string.IsNullOrEmpty(searchTerm))
+        //for text search
+        // var query = DB.Find<Item>();
+
+        //for pagination
+        var query = DB.PagedSearch<Item, Item>();
+
+        if (!string.IsNullOrEmpty(searchParams.searchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.searchTerm).SortByTextScore();
         }
+
+        //order
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        //filter
+        query = searchParams.FilterBy switch
+        {
+            //if auction ends before this day
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            //items that are about tp be finished in 6 hours
+            "endingSoon" => query.Match(x => x.AuctionEnd > DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        //if seller has input, then display only the items of that seller
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        //pagination parameters(only available in pagination)
+        query.PageNumber(searchParams.pageNumber);
+        query.PageSize(searchParams.pageSize);
+
         var result = await query.ExecuteAsync();
-        return result;
+        return Ok(new
+        {
+            //results
+            results = result.Results,
+            //to know how many pages the frontend need to render in pagination ui
+            pageCount = result.PageCount,
+            //how many results total
+            totalCount = result.TotalCount
+        });
     }
 }
