e6cd6254a278a0011a237dad1589aa79abafd653
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-02-20 10:17:16 +0800

    nextApp client added

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -29,7 +29,25 @@ public static class Config
                 AllowedScopes =  { "openid", "profile", "auctionApp" },
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new [] {new Secret("NotSecret".Sha256())},
+                //password grant allows auth to service by password which is dangerous
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client{
+                ClientId= "nextApp",
+                ClientName= "nextAppp",
+                ClientSecrets={new Secret("secret".Sha256())},
+                //requires authorization code
+                AllowedGrantTypes= GrantTypes.CodeAndClientCredentials,
+                //additional security
+                //for mobile app make it true
+                RequirePkce=false,
+                RedirectUris={"http://localhost:3000/api/auth/callback/id-server"},
+                //for refresh token
+                AllowOfflineAccess=true,
+                AllowedScopes =  { "openid", "profile", "auctionApp" },
+                //the default is 1 hour
+                //make it one month
+                AccessTokenLifetime = 3600*24*30
             }
         };
 }
