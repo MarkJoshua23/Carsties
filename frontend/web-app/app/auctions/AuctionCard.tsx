45c8de06f8ff201703702eb19f4eba4f2dddb8fd
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-02-24 16:55:09 +0800

    pagination pt1

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,34 +1,31 @@
-import Image from 'next/image'
-import React from 'react'
-import CountdownTimer from './CountdownTimer'
-import CardImage from './CardImage'
-
-type Props ={
-    auction: any
-}
+import Image from "next/image";
+import React from "react";
+import CountdownTimer from "./CountdownTimer";
+import CardImage from "./CardImage";
+import { Auction } from "@/types";
+//use type Auction so its like DTO
+type Props = {
+  auction: Auction;
+};
 //image fill will fill up the container with the image
 //object cover makes the image cover the aspect ratio and not stretch
-export default function AuctionCard({auction} : Props) {
+export default function AuctionCard({ auction }: Props) {
   //relative so the image will base its scale on the div
   //countdown is absolute so it can be stacked above, since theres no available space bc the image filled all space
   return (
-    <a href="#" className='group'>
-      
+    <a href="#" className="group">
       <div className="relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden">
-        <CardImage imageUrl={auction.imageUrl}/>
-        <div className='absolute bottom-2 left-2'>
-        <CountdownTimer auctionEnd={auction.auctionEnd}/>
+        <CardImage imageUrl={auction.imageUrl} />
+        <div className="absolute bottom-2 left-2">
+          <CountdownTimer auctionEnd={auction.auctionEnd} />
         </div>
       </div>
       <div className="flex justify-between items-center mt-4">
-        <h3 className='text-gray-700 '>
+        <h3 className="text-gray-700 ">
           {auction.make} {auction.model}
         </h3>
-        <p className='font-semibold text-sm'>
-          {auction.year}
-        </p>
+        <p className="font-semibold text-sm">{auction.year}</p>
       </div>
-      
     </a>
-  )
+  );
 }
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import AuctionCard from './AuctionCard';
-import { Auction, PagedResult } from '@/types';
+import React from "react";
+import AuctionCard from "./AuctionCard";
+import { Auction, PagedResult } from "@/types";
+import AppPagination from "../components/AppPagination";
 
 //this will also cache
 //it will promise of return of pagedresult with items including a type of auction
 async function getData(): Promise<PagedResult<Auction>> {
-    const res = await fetch('http://localhost:6001/search?pageSize=10',{
-        next: { revalidate: 60 } // Cache for 60 seconds, then refresh
-    });
-    if (!res.ok) throw new Error('Failed to fetch data');
-    //get the respose from res
-    return res.json();
+  const res = await fetch("http://localhost:6001/search?pageSize=4", {
+    next: { revalidate: 60 }, // Cache for 60 seconds, then refresh
+  });
+  if (!res.ok) throw new Error("Failed to fetch data");
+  //get the respose from res
+  return res.json();
 }
 
 export default async function Listings() {
-    //store the fetched data to data
-    const data = await getData();
+  //store the fetched data to data
+  const data = await getData();
 
   return (
-    <div className='grid lg:grid-cols-4 sm:grid-cols-2 gap-6'>
-        {data && data.results.map((auction, index)=>(
-          <AuctionCard auction={auction} key={index}/>
-        ))}
-    </div>
-  )
+    <>
+      <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
+        {data &&
+          data.results.map((auction, index) => (
+            <AuctionCard auction={auction} key={index} />
+          ))}
+      </div>
+      <div className="flex justify-center mt-4 ">
+        <AppPagination currentPage={1} pageCount={data.pageCount} />
+      </div>
+    </>
+  );
 }
